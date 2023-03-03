8d0ed2d71156f882fe35f42dafac787750400a33
hanhdt268 <nguyenvanhanh686820@gmail.com>
2023-03-03 17:23:56 +0700

    jmeter

--- UserService/src/main/java/com/userservice/controller/UserController.java
+++ UserService/src/main/java/com/userservice/controller/UserController.java
@@ -4,6 +4,8 @@ import com.userservice.entity.User;
 import com.userservice.service.Impl.UserServiceImpl;
 import com.userservice.service.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,15 +35,21 @@ public class UserController {
         return ResponseEntity.ok(this.userService.getAllUser());
     }
 
+    int retryCount = 1;
     @GetMapping("/{userId}")
-    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+//    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+//    @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
+    @RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<?> getUserById(@PathVariable("userId") String userId){
+        logger.info("Get single User Handle: UserController");
+        logger.info("Retry count: {}", this.retryCount);
+        this.retryCount ++;
         return ResponseEntity.ok(this.userService.getUserById(userId));
     }
 
     //creating fallback method for circuitBreaker
     public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex){
-        logger.info("fallback is executed because service down: ", ex.getMessage());
+//        logger.info("fallback is executed because service down: ", ex.getMessage());
         User user = User.builder().userId("123123").name("Chery").email("chery@gmail.com").about("This is create user " +
                 "chery because some service is down").build();
         return new ResponseEntity<>(user, HttpStatus.OK);
