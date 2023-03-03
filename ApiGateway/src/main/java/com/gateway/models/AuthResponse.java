db10268790fd47888be054dfece21caed68347e9
hanhdt268 <nguyenvanhanh686820@gmail.com>
2023-03-03 17:22:50 +0700

    Security Implement using OKTA

---
+++ ApiGateway/src/main/java/com/gateway/models/AuthResponse.java
@@ -0,0 +1,20 @@
+package com.gateway.models;
+
+import lombok.*;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String userId;
+    private String accessToken;
+    private String refreshToken;
+
+    private Long expireAt;
+    private Collection<String> authorities;
+}
